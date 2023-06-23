423c2c57a86cd1d1b8e3670421cc4f9c6c4d78bf
sotin21 <s.soto15@ufromail.cl>
2023-06-23 10:29:51 -0400

    como funcionaria la eleccion de pantalla en los computadores

---
+++ src/main/java/Pantalla.java
@@ -0,0 +1,32 @@
+class Pantalla {
+    private String marca;
+    private String modelo;
+    private int año;
+
+    public Pantalla() {
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+}
